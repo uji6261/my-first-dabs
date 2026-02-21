9bffbc90fb1ec82a77873c8f0a58baf27d4be620
s-ujihara <londonkick@yahoo.co.jp>
2026-02-21 13:46:07 +0000

    update sst001

--- src/pj-main/pj_01/SST/SST001.py
+++ src/pj-main/pj_01/SST/SST001.py
@@ -1,21 +1,11 @@
 # Databricks notebook source
-# MAGIC %run ../config/_env_config
+notebook_name = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get().split("/")[-1]
+
+print(notebook_name)
 
 # COMMAND ----------
 
 dbutils.widgets.text("ENV", "local_test")
 env = dbutils.widgets.get("ENV")
 
-notebook_name = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get().split("/")[-1]
-config = load_config("../config/config.json", notebook_name)
-
-print(config)
-print(config["source_table"])
-print(config["target_table"])
-
-# COMMAND ----------
-
-print("SST001を実行しました")
 print(env)
-
-print("Git Folder作成しました")
